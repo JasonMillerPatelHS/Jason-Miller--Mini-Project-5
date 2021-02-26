5d82e561cc8a2b07f0c4af845215632bbe35ca5d
JasonMillerPHS <jason.miller@patelhs.org>
2021-02-26 03:09:31 +0000

    Made return value necessary, added return statements and end scanners

--- Main.java
+++ Main.java
@@ -5,8 +5,20 @@ class Main {
     Scanner scan = new Scanner(System.in); //declaring Scanner scan
     System.out.println("Welcome to Jason's Marcket! This program is a price calculator! \nWhat is your budget for spending?");
     double budget = scan.nextDouble(); //uses scanner scan
+    double beforeBudget = budget;
     budget = money(budget); //calls to method, makes budget = to method
     System.out.println(budget); //prints final budget
+    double spentReq = beforeBudget - 40;
+    if (spentReq >= budget)
+    {
+      System.out.println("You qualified for our discount on purchases over $40, \nyou get $20 off! \nyour cash left over is now: ");
+      budget = budget + 20; //give $20 back
+      System.out.println(budget);
+    }
+    else
+    {
+      System.out.println("You do not qualify for the $20 discount");
+    }
     scan.close(); //ends scanner in main
   } 
 
@@ -15,9 +27,9 @@ class Main {
     Scanner scan = new Scanner(System.in); //scanner scan was not declared in method money.
     System.out.println("How much will your item be? (input without a $ symbol)");
     double price = scan.nextDouble(); //price is the cost of customers item purchased
-    int purchase = 1; //purchase is the measure so the user knows how many items they 'purchased'
+    int purchase = 0; //purchase is the measure so the user knows how many items they 'purchased'
 
-    if (cash <= price) //if item is too expensive then...
+    if (cash < price) //if item is too expensive then...
     {
       System.out.println("You don't have enough money; \n would you like to try to input a cheaper item? (y/n)");
 
@@ -29,11 +41,12 @@ class Main {
       } 
       else if (response.equals("n"))
       {
-        System.out.println("This program ended, \n if you made a mistake along the way, simply restart the program. Balance($): ");
+        System.out.println("If you made a mistake along the way, simply restart the program. Your balance($): ");
+        scan.close(); //ends scanner
         return cash;
       } 
     } 
-    else if (cash > price && price >= 0)
+    else if (cash >= price && price >= 0)
     {
       double newCash = cash - price; 
       purchase++; //adds one to purchases to show how many items user bought
@@ -42,7 +55,8 @@ class Main {
       String response = scan.next(); //scans user response again
       if (response.equals("y")) //if user typed y then..
         {
-          System.out.println("Okay, thank you for shopping with Jason! \nIf you made a mistake along the way restart the program! \nfinal balance ($): ");
+          System.out.println("Okay, thank you for shopping with Jason! \nIf you made a mistake along the way restart the program! \nCash left over($): ");
+          scan.close(); //ends scanner
           return newCash;
         } 
       else if (response.equals("n")) //if user typed n...
@@ -52,11 +66,13 @@ class Main {
       } 
       else
       {
+        scan.close(); //ends scanner
         return newCash;
       }
       scan.close(); //ends scanner
+      return newCash;
     } 
-    scan.close(); //ends scanner in method
+    scan.close(); //ends scanner
     return cash;
   } 
 } 
